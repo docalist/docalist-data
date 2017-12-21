301ddecda37010694bfbd79fbeef9ba73b3578f8
Daniel Ménard <daniel.menard@laposte.net>
2017-12-21 11:24:06 +0100

    Modifie le fichier de démarrage.

--- docalist-databases.php
+++ docalist-databases.php
@@ -1,31 +1,31 @@
 <?php
 /**
- * This file is part of the 'Docalist Biblio' plugin.
+ * This file is part of the 'Docalist Databases' plugin.
  *
  * Copyright (C) 2012-2017 Daniel Ménard
  *
  * For copyright and license information, please view the
  * LICENSE.txt file that was distributed with this source code.
  *
- * Plugin Name: Docalist Biblio
+ * Plugin Name: Docalist Databases
  * Plugin URI:  http://docalist.org/
- * Description: Docalist: bibliographic data manager.
- * Version:     0.15.0
+ * Description: Docalist: gestion des bases.
+ * Version:     0.16.0
  * Author:      Daniel Ménard
  * Author URI:  http://docalist.org/
- * Text Domain: docalist-biblio
+ * Text Domain: docalist-databases
  * Domain Path: /languages
  *
  * @package     Docalist
- * @subpackage  Biblio
+ * @subpackage  Databases
  * @author      Daniel Ménard <daniel.menard@laposte.net>
  */
-namespace Docalist\Biblio;
+namespace Docalist\Databases;
 
 /**
  * Version du plugin.
  */
-define('DOCALIST_BIBLIO_VERSION', '0.15.0'); // Garder synchro avec la version indiquée dans l'entête
+define('DOCALIST_DATABASES_VERSION', '0.16.0'); // Garder synchro avec la version indiquée dans l'entête
 
 /**
  * Path absolu du répertoire dans lequel le plugin est installé.
@@ -36,17 +36,17 @@ define('DOCALIST_BIBLIO_VERSION', '0.15.0'); // Garder synchro avec la version i
  * Si le répertoire du plugin est un lien symbolique, la constante doit être définie manuellement dans le fichier
  * wp_config.php et pointer sur le lien symbolique et non sur le répertoire réel.
  */
-!defined('DOCALIST_BIBLIO_DIR') && define('DOCALIST_BIBLIO_DIR', __DIR__);
+!defined('DOCALIST_DATABASES_DIR') && define('DOCALIST_DATABASES_DIR', __DIR__);
 
 /**
  * Path absolu du fichier principal du plugin.
  */
-define('DOCALIST_BIBLIO', DOCALIST_BIBLIO_DIR . DIRECTORY_SEPARATOR . basename(__FILE__));
+define('DOCALIST_DATABASES', DOCALIST_DATABASES_DIR . DIRECTORY_SEPARATOR . basename(__FILE__));
 
 /**
  * Url de base du plugin.
  */
-define('DOCALIST_BIBLIO_URL', plugins_url('', DOCALIST_BIBLIO));
+define('DOCALIST_DATABASES_URL', plugins_url('', DOCALIST_DATABASES));
 
 /**
  * Initialise le plugin.
@@ -57,12 +57,12 @@ add_action('plugins_loaded', function () {
     foreach ($dependencies as $dependency) {
         if (! defined($dependency)) {
             return add_action('admin_notices', function () use ($dependency) {
-                deactivate_plugins(DOCALIST_BIBLIO);
+                deactivate_plugins(DOCALIST_DATABASES);
                 unset($_GET['activate']); // empêche wp d'afficher "extension activée"
                 printf(
                     '<div class="%s"><p><b>%s</b> has been deactivated because it requires <b>%s</b>.</p></div>',
                     'notice notice-error is-dismissible',
-                    'Docalist Biblio',
+                    'Docalist Databases',
                     ucwords(strtolower(strtr($dependency, '_', ' ')))
                 );
             });
@@ -74,13 +74,13 @@ add_action('plugins_loaded', function () {
         ->add(__NAMESPACE__, __DIR__ . '/class')
         ->add(__NAMESPACE__ . '\Tests', __DIR__ . '/tests');
 
-    docalist('services')->add('docalist-biblio', new Plugin());
+    docalist('services')->add('docalist-databases', new Plugin());
 });
 
 /*
  * Activation du plugin.
  */
-register_activation_hook(DOCALIST_BIBLIO, function () {
+    register_activation_hook(DOCALIST_DATABASES, function () {
     // Si docalist-core n'est pas dispo, on ne peut rien faire
     if (defined('DOCALIST_CORE')) {
         // plugins_loaded n'a pas encore été lancé, donc il faut initialiser notre autoloader
@@ -92,7 +92,7 @@ register_activation_hook(DOCALIST_BIBLIO, function () {
 /*
  * Désactivation du plugin.
 */
-register_deactivation_hook(DOCALIST_BIBLIO, function () {
+register_deactivation_hook(DOCALIST_DATABASES, function () {
     // Si docalist-core n'est pas dispo, on ne peut rien faire
     if (defined('DOCALIST_CORE')) {
         docalist('autoloader')->add(__NAMESPACE__, __DIR__ . '/class');
