667a7c2c19157537e9ebeff1123a70a1b00bd6b5
Daniel Ménard <daniel.menard@laposte.net>
2017-12-22 14:57:41 +0100

    Réintroduit la classe Type (compatibilité ascendante)

---
+++ class/Type.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * This file is part of Docalist Databases.
+ *
+ * Copyright (C) 2012-2017 Daniel Ménard
+ *
+ * For copyright and license information, please view the
+ * LICENSE.txt file that was distributed with this source code.
+ */
+namespace Docalist\Databases;
+
+use Docalist\Databases\Record;
+
+/**
+ * Compatibilité ascendante : ancien nom de la classe Record.
+ */
+class Type extends Record
+{
+}
