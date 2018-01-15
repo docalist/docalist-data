ac7dae8247b35fdf6dbbd33cb69c27c1379ba43f
Daniel Ménard <daniel.menard@laposte.net>
2018-01-15 15:40:03 +0100

    Corrige les entêtes (nom du plugin et année)

--- assets/register.php
+++ assets/register.php
@@ -1,8 +1,8 @@
 <?php
 /**
- * This file is part of Docalist Databases.
+ * This file is part of Docalist Data.
  *
- * Copyright (C) 2012-2017 Daniel Ménard
+ * Copyright (C) 2012-2018 Daniel Ménard
  *
  * For copyright and license information, please view the
  * LICENSE.txt file that was distributed with this source code.
