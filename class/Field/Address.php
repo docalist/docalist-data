7d18e062af4a20d59ac8f5cd08d042c7b7d8ee65
Daniel Ménard <daniel.menard@laposte.net>
2018-01-29 11:08:01 +0100

    Champ standard address

---
+++ class/Field/Address.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * This file is part of Docalist Data.
+ *
+ * Copyright (C) 2012-2018 Daniel Ménard
+ *
+ * For copyright and license information, please view the
+ * LICENSE.txt file that was distributed with this source code.
+ */
+namespace Docalist\Data\Field;
+
+use Docalist\Data\Type\TypedPostalAddress;
+
+/**
+ * Champ standard "address" : adresse postale.
+ *
+ * Ce champ permet de saisir les adresses postales d'une entité.
+ *
+ * Chaque occurence comporte deux sous-champs :
+ * - `type` : type d'adresse,
+ * - `value` : adresse.
+ *
+ * Le sous-champ type est associé à une table d'autorité qui indique les types d'adresses disponibles
+ * ("table:address-type" par défaut).
+ *
+ * @author Daniel Ménard <daniel.menard@laposte.net>
+ */
+class Address extends TypedPostalAddress
+{
+}
