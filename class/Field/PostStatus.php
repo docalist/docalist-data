301dfbe6dcd7bfb648a37101d62cecf3fe2b007c
Daniel Ménard <daniel.menard@laposte.net>
2018-01-29 11:12:07 +0100

    Transfère PostStatus de Type vers Field

--- class/Field/PostStatus.php
+++ class/Field/PostStatus.php
@@ -7,12 +7,14 @@
  * For copyright and license information, please view the
  * LICENSE.txt file that was distributed with this source code.
  */
-namespace Docalist\Data\Type;
+namespace Docalist\Data\Field;
 
 use Docalist\Type\Text;
 
 /**
- * Le statut wordpress de la notice.
+ * Champ standard "status" : statut de visibilité de l'enregistrement.
+ *
+ * Ce champ Docalist correspond au champ WordPress "post_status".
  *
  * @author Daniel Ménard <daniel.menard@laposte.net>
  */
