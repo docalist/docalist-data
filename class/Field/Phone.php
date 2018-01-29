ce32f829396d7836de1e34c8ec494a3c3f0e930c
Daniel Ménard <daniel.menard@laposte.net>
2018-01-29 11:09:23 +0100

    Champ standard phone

---
+++ class/Field/Phone.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * This file is part of Docalist Data.
+ *
+ * Copyright (C) 2012-2018 Daniel Ménard
+ *
+ * For copyright and license information, please view the
+ * LICENSE.txt file that was distributed with this source code.
+ */
+namespace Docalist\Data\Field;
+
+use Docalist\Data\Type\TypedPhoneNumber;
+
+/**
+ * Champ standard "phone" : numéros de téléphone.
+ *
+ * Ce champ permet de saisir les numéros de téléphones d'une entité.
+ *
+ * Chaque numéro comporte deux sous-champs :
+ * - `type` : type de numéro,
+ * - `value` : numéro de téléphone.
+ *
+ * Le sous-champ type est associé à une table d'autorité qui indique les types de numéros disponibles
+ * ("table:phone-number" par défaut).
+ *
+ * @author Daniel Ménard <daniel.menard@laposte.net>
+ */
+class Phone extends TypedPhoneNumber
+{
+}
