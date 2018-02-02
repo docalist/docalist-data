a12d0dcbf9f84f18f7f2e30ca4361d67b84f5e86
Daniel Ménard <daniel.menard@laposte.net>
2018-02-02 14:57:12 +0100

    assets/register : permet de définir API key et langue pour google maps
    - la clé d'API google est maintenant obligatoire
    - on regarde si la constante GOOGLE_API_KEY est définie (à indiquer dans
    wp-config).
    - on gère également la constante GOOGLE_PLACES_LANGUAGE pour charger
    l'API Google Places Autocomplete

--- assets/register.php
+++ assets/register.php
@@ -13,12 +13,12 @@ namespace Docalist\Data;
 
 // Les scripts suivants ne sont dispos que dans le back-office
 add_action('admin_init', function () {
-    $url = DOCALIST_DATA_URL;
+    $base = DOCALIST_DATA_URL;
 
     // Css pour EditReference (également utilisé dans le paramétrage de la grille de saisie)
     wp_register_style(
         'docalist-data-edit-reference',
-        "$url/assets/edit-reference.css",
+        "$base/assets/edit-reference.css",
         ['wp-admin'],
         '160108'
     );
@@ -26,7 +26,7 @@ add_action('admin_init', function () {
     // Editeur de grille
     wp_register_script(
         'docalist-data-grid-edit',
-        "$url/views/grid/edit.js",
+        "$base/views/grid/edit.js",
         ['jquery', 'jquery-ui-sortable'],
         '20150510',
         true
@@ -34,7 +34,7 @@ add_action('admin_init', function () {
 
     wp_register_style(
         'docalist-data-grid-edit',
-        "$url/views/grid/edit.css",
+        "$base/views/grid/edit.css",
         [],
         '20150510'
     );
@@ -42,23 +42,26 @@ add_action('admin_init', function () {
     // Adresse Postale
     wp_register_style(
         'docalist-postal-address',
-        "$url/assets/postal-address/postal-address.css",
+        "$base/assets/postal-address/postal-address.css",
         ['wp-admin'],
         '151229'
     );
+
+    $url = 'https://maps.googleapis.com/maps/api/js?libraries=places';
+    defined('GOOGLE_PLACES_LANGUAGE') && $url .= '&language='  . rawurlencode(GOOGLE_PLACES_LANGUAGE); // dans wp-config
+    defined('GOOGLE_API_KEY') && $url .= '&key=' . rawurlencode(GOOGLE_API_KEY); // dans wp-config
     wp_register_script(
         'google-maps-places',
-        '//maps.googleapis.com/maps/api/js?libraries=places',
+        $url,
         [],
-        '20151229',
+        null, // Ne pas générer de numéro de version dans l'url du script, google gère lui-même son versionning
         true
     );
     wp_register_script(
         'docalist-postal-address',
-        "$url/assets/postal-address/postal-address.js",
+        "$base/assets/postal-address/postal-address.js",
         ['jquery', 'google-maps-places'],
         '20151229',
         true
     );
-
 });
