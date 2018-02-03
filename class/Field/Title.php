e6ed0b5757bac7c8cca6439912e3611de99af56a
Daniel Ménard <daniel.menard@laposte.net>
2018-02-03 11:08:23 +0100

    Nouveau champ standard Field/Title
    Se contente d'hériter de Type/PostTitle

---
+++ class/Field/Title.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * This file is part of Docalist Data.
+ *
+ * Copyright (C) 2012-2018 Daniel Ménard
+ *
+ * For copyright and license information, please view the
+ * LICENSE.txt file that was distributed with this source code.
+ */
+namespace Docalist\Data\Field;
+
+use Docalist\Data\Field\PostTitle;
+
+/**
+ * Champ standard "title" : le titre d'un contenu.
+ *
+ * Ce champ permet de saisir des tags et des mots-clés pour une entité.
+ *
+ * Chaque occurence comporte deux sous-champs :
+ * - `type` : type d'indexation,
+ * - `value` : listes des mots-clés pour ce type d'indexation.
+ *
+ * Le sous-champ type est associé à une table d'autorité qui indique les différents types d'indexation disponibles
+ * ("table:topic-type" par défaut).
+ *
+ * @author Daniel Ménard <daniel.menard@laposte.net>
+ */
+class Title extends PostTitle
+{
+    public static function loadSchema()
+    {
+        return [
+            'label' => __('Titre', 'docalist-data'),
+            'description' => __('-', 'docalist-data'),
+        ];
+    }
+}
