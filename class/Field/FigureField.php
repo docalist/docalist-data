8dc91d166f08552b2f8acead08c5604b2146fd55
Daniel Ménard <daniel.menard@laposte.net>
2018-02-04 14:10:49 +0100

    Nouveau champ standard FigureField : chiffres clés

---
+++ class/Field/FigureField.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * This file is part of Docalist Data.
+ *
+ * Copyright (C) 2012-2018 Daniel Ménard
+ *
+ * For copyright and license information, please view the
+ * LICENSE.txt file that was distributed with this source code.
+ */
+namespace Docalist\Data\Field;
+
+use Docalist\Type\TypedDecimal;
+
+/**
+ * Champ standard "figure" : chiffres clés.
+ *
+ * Ce champ permet de saisir des chiffres clés (taille, surface, nombre de publications, effectifs,
+ * chiffre d'affaires, implantations...)
+ *
+ * Chaque numéro comporte deux sous-champs :
+ * - `type` : type de chiffre clé,
+ * - `value` : valeur.
+ *
+ * Le sous-champ type est associé à une table d'autorité qui indique les types de chiffres-clés disponibles
+ * ("table:figure-type" par défaut).
+ *
+ * @author Daniel Ménard <daniel.menard@laposte.net>
+ */
+class FigureField extends TypedDecimal
+{
+    public static function loadSchema()
+    {
+        return [
+            'label' => __('Chiffres clés', 'docalist-data'),
+            'description' => __("Chiffres clés et données factuelles.", 'docalist-data'),
+            'fields' => [
+                'type' => [
+                    'table' => 'table:figure-type',
+                ],
+            ],
+        ];
+    }
+}
