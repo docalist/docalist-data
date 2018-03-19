cd5fb6f33f6c4a153a8941cb4bc4be8fe0ee2eeb
Daniel Ménard <daniel.menard@laposte.net>
2018-03-19 09:51:22 +0100

    Nouvelle interface ExportException
    Classe de base pour les exceptions générées lors de l'export.

---
+++ class/Export/ExportException.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * This file is part of Docalist Data.
+ *
+ * Copyright (C) 2012-2018 Daniel Ménard
+ *
+ * For copyright and license information, please view the
+ * LICENSE.txt file that was distributed with this source code.
+ */
+namespace Docalist\Data\Export;
+
+use Exception;
+
+/**
+ * Classe de base pour les exceptions générées lors de l'export de données Docalist.
+ *
+ * @author Daniel Ménard <daniel.menard@laposte.net>
+ */
+class ExportException extends Exception
+{
+}
