d1395387523412c8e6830bf90843018cf792ff52
Daniel Ménard <daniel.menard@laposte.net>
2018-03-19 10:23:07 +0100

    Nouvelle interface Exporter
    Convertit des enregistrements Docalist et les écrit dans un flux de
    sortie.
    
    Remplace l'ancienne classe Exporter.

--- class/Export/Exporter.php
+++ class/Export/Exporter.php
@@ -9,117 +9,35 @@
  */
 namespace Docalist\Data\Export;
 
-use Traversable;
-use Docalist\Data\Record;
+use Docalist\Data\Export\ExportWriter;
 
 /**
- * Classe de base pour les exporteurs.
+ * Interface d'un exporteur Docalist.
  *
- * Un exporteur travaille de concert avec un convertisseur pour exporter
- * des notices dans un format donné.
- *
- * Pour chacune des notices, il appelle la méthode convert() du convertisseur
- * et écrit les données obtenus dans le format de fichier attendu.
+ * Un exporteur convertit des enregistrements Docalist et les écrit dans un flux de sortie.
  *
  * @author Daniel Ménard <daniel.menard@laposte.net>
  */
-class Exporter extends BaseExport
+interface Exporter extends ExportWriter
 {
-    // Paramétres communs à tous les exporteurs
-    protected static $defaultSettings = [
-        // Disposition du fichier d'export généré ('inline' ou 'attachment')
-        'disposition' => 'inline',
-
-        // Type MIME du fichier d'export généré
-        'mime-type' => 'text/plain',
-
-        // Charset du fichier généré
-        'charset' => 'utf-8',
-
-        // Nom de base du fichier exporté (sans extension).
-        'filename' => 'export',
-
-        // Extension du fichier
-        'extension' => '.txt',
-
-        // Indique s'il s'agit d'un fichier binaire.
-        // Si false , l'option "afficher les notices" sera proposée lors de l'export.
-        'binary' => false,
-    ];
+    /**
+     * Retourne un identifiant unique pour l'exporteur.
+     *
+     * @return string
+     */
+    public static function getID();
 
     /**
-     * Le convertisseur à utiliser.
+     * Retourne le libellé de l'exporteur.
      *
-     * @var Converter
+     * @return string
      */
-    protected $converter;
+    public static function getLabel();
 
     /**
-     * Initialise l'exporteur.
+     * Retourne la description de l'exporteur.
      *
-     * @param Converter $converter Le convertisseur à utiliser.
-     *
-     * @param array $settings Les paramètres de l'exporteur.
+     * @return string
      */
-    public function __construct(Converter $converter = null, array $settings = [])
-    {
-        parent::__construct($settings);
-        $this->converter = $converter;
-    }
-
-    /**
-     * Exporte les enregistrements passés en paramètre.
-     *
-     * @param Traversable|Record[] $records Les enregistrements à exporter.
-     */
-    public function export($records)
-    {
-        foreach ($records as $record) { /** var Record $ref */
-            $data = $this->converter->convert($record);
-            var_export($data);
-            echo "\n\n";
-        }
-    }
-
-    /**
-     * Retourne l'entête "content-type" de la réponse http générée lors de
-     * l'export.
-     *
-     * L'entête est généré en combinant le résultat des méthodes mimeType() et
-     * charset().
-     *
-     * @return string Par défaut, retourne la chaine
-     * "text/plain; charset=utf-8"
-     */
-    final public function contentType()
-    {
-        return sprintf('%s; charset=%s', $this->get('mime-type'), $this->get('charset'));
-    }
-
-    /**
-     * Retourne l'entête "content-disposition" de la réponse http générée lors
-     * de l'export.
-     *
-     * L'entête est généré en combinant le résultat des méthodes inline(),
-     * filename() et extension().
-     *
-     * @return string Par défaut, retourne la chaine
-     * "inline; filename=export.txt"
-     */
-    final public function contentDisposition($disposition = null)
-    {
-        is_null($disposition) && $disposition = $this->get('disposition');
-        $extension = '.' . trim($this->get('extension'), '. ');
-        $filename = $this->get('filename');
-        $fallback = sanitize_title($filename) . $extension;
-        $filename .= $extension;
-
-        $header = sprintf('%s; filename="%s"', $disposition, $fallback);
-
-        if ($filename !== $fallback) {
-            $header .= sprintf("; filename*=utf-8''%s", rawurlencode($filename));
-        }
-
-        return $header;
-    }
+    public static function getDescription();
 }
