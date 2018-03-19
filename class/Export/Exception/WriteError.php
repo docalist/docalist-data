245fbed51fec5bb85ec33ed1af0b917c437457db
Daniel Ménard <daniel.menard@laposte.net>
2018-03-19 09:52:00 +0100

    Nouvelle exception WriteError

---
+++ class/Export/Exception/WriteError.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * This file is part of Docalist Data.
+ *
+ * Copyright (C) 2012-2018 Daniel Ménard
+ *
+ * For copyright and license information, please view the
+ * LICENSE.txt file that was distributed with this source code.
+ */
+namespace Docalist\Data\Export\Converter;
+
+use Docalist\Data\Export\ExportException;
+
+/**
+ * Une exception indiquant qu'une erreur est survenue lors de l'écriture des données exportées.
+ *
+ * @author Daniel Ménard <daniel.menard@laposte.net>
+ */
+class WriteError extends ExportException
+{
+}
