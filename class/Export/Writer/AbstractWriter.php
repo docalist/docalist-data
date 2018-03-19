a67da5f5552832ddcaed73ba2bec34ea1386b0a0
Daniel Ménard <daniel.menard@laposte.net>
2018-03-19 15:52:32 +0100

    Nouvelle classe AbstractWriter
    Pour avoir une classe commune pour les Writer.
    
    Implémente exportToString

---
+++ class/Export/Writer/AbstractWriter.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * This file is part of Docalist Data.
+ *
+ * Copyright (C) 2012-2018 Daniel Ménard
+ *
+ * For copyright and license information, please view the
+ * LICENSE.txt file that was distributed with this source code.
+ */
+namespace Docalist\Data\Export\Writer;
+
+use Docalist\Data\Export\Writer;
+
+/**
+ * Classe de base pour les Writers.
+ *
+ * @author Daniel Ménard <daniel.menard@laposte.net>
+ */
+abstract class AbstractWriter implements Writer
+{
+    public function exportToString(Iterable $records)
+    {
+        // Ouvre un flux en mémoire mémoire
+        $stream = fopen('php://temp','r+');
+
+        // Exporte les enregistrements
+        $this->export($stream, $records);
+
+        // Rembobine le flux et récupère tout ce qui a été généré
+        rewind($stream);
+        $result = stream_get_contents($stream);
+
+        // Ferme le flux
+        fclose($stream);
+
+        // Retourne le résultat
+        return $result;
+    }
+}
