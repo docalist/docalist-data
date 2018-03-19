3e5d4a48cd62da1685d00d44b2e5daa7b8f9e2da
Daniel Ménard <daniel.menard@laposte.net>
2018-03-19 20:19:55 +0100

    StandardExporters : liste les exporteurs disponibles

--- class/Export/Exporter/StandardExporters.php
+++ class/Export/Exporter/StandardExporters.php
@@ -7,74 +7,30 @@
  * For copyright and license information, please view the
  * LICENSE.txt file that was distributed with this source code.
  */
-namespace Docalist\Data\Export;
+namespace Docalist\Data\Export\Exporter;
 
-use Docalist\Data\Export\Docalist;
-use Docalist\Data\Export\Json;
-use Docalist\Data\Export\Xml;
+use Docalist\Data\Export\Exporter\DocalistJson;
+use Docalist\Data\Export\Exporter\DocalistXml;
 
 /**
- * Gère la liste des formats d'export prédéfinis de docalist-data.
+ * Gère la liste des exports prédéfinis de docalist-data.
  *
  * @author Daniel Ménard <daniel.menard@laposte.net>
  */
-class PredefinedExportFormats
+class StandardExporters
 {
     /**
-     * Retourne la liste des formats d'export prédéfinis de docalist-data.
+     * Retourne la liste des exports prédéfinis de docalist-data.
      *
      * @return array[] Un tableau de la forme format-name => settings.
      */
     public static function getList()
     {
-        $formats = [];
-
-        $formats['docalist-json'] = [
-            'label' => 'Docalist JSON',
-            'description' => __(
-                'Fichier JSON compact, contenus au format natif de Docalist.',
-                'docalist-data'
-            ),
-            'converter' => Docalist::class,
-            'exporter' => Json::class,
+        return [
+            DocalistJson::getID()       => DocalistJson::class,
+            DocalistJsonPretty::getID() => DocalistJsonPretty::class,
+            DocalistXml::getID()        => DocalistXml::class,
+            DocalistXmlPretty::getID()  => DocalistXmlPretty::class,
         ];
-
-        $formats['docalist-json-pretty'] = [
-            'label' => 'Docalist JSON formatté',
-            'description' => __(
-                'Fichier JSON formatté et indenté, contenus au format natif de Docalist.',
-                'docalist-data'
-            ),
-            'converter' => Docalist::class,
-            'exporter' => Json::class,
-            'exporter-settings' => [
-                'pretty' => true,
-            ],
-        ];
-
-        $formats['docalist-xml'] = [
-            'label' => 'Docalist XML',
-            'description' => __(
-                'Fichier XML compact, contenus au format natif de Docalist.',
-                'docalist-data'
-            ),
-            'converter' => Docalist::class,
-            'exporter' => Xml::class,
-        ];
-
-        $formats['docalist-xml-pretty'] = [
-            'label' => 'Docalist XML formatté',
-            'description' => __(
-                'Fichier XML formatté et indenté, contenu au format natif de Docalist.',
-                'docalist-data'
-            ),
-            'converter' => Docalist::class,
-            'exporter' => Xml::class,
-            'exporter-settings' => [
-                'indent' => 4,
-            ],
-        ];
-
-        return $formats;
     }
 }
