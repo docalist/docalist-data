f7c5ff3749d871a9d7389eb9e19af7e5e7bd7945
Daniel Ménard <daniel.menard@laposte.net>
2018-03-20 10:42:06 +0100

    Tests unitaires : StandardExportersTest

---
+++ tests/Export/Exporter/StandardExportersTest.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * This file is part of Docalist Data.
+ *
+ * Copyright (C) 2012-2018 Daniel Ménard
+ *
+ * For copyright and license information, please view the
+ * LICENSE.txt file that was distributed with this source code.
+ */
+namespace Docalist\Data\Tests\Export\Exporter;
+
+use WP_UnitTestCase;
+use Docalist\Data\Export\Exporter\StandardExporters;
+use Docalist\Data\Export\Exporter\DocalistJson;
+use Docalist\Data\Export\Exporter\DocalistJsonPretty;
+use Docalist\Data\Export\Exporter\DocalistXml;
+use Docalist\Data\Export\Exporter\DocalistXmlPretty;
+
+/**
+ * Teste la liste des formats disponibles.
+ *
+ * @author Daniel Ménard <daniel.menard@laposte.net>
+ */
+class StandardExportersTest extends WP_UnitTestCase
+{
+    /**
+     * Teste la méthode getList()
+     */
+    public function testGetList()
+    {
+        $list = StandardExporters::getList();
+
+        $this->assertSame(4, count($list));
+        $this->assertTrue(isset($list[DocalistJson::getID()]));
+        $this->assertTrue(isset($list[DocalistJsonPretty::getID()]));
+        $this->assertTrue(isset($list[DocalistXml::getID()]));
+        $this->assertTrue(isset($list[DocalistXmlPretty::getID()]));
+    }
+}
