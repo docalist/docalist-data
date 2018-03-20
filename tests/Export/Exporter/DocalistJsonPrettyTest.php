1fc702a3db7fc2b57d984d7e52e12648a4389a34
Daniel Ménard <daniel.menard@laposte.net>
2018-03-20 09:07:26 +0100

    Tests unitaires : DocalistJsonPrettyTest

---
+++ tests/Export/Exporter/DocalistJsonPrettyTest.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * This file is part of Docalist Data.
+ *
+ * Copyright (C) 2012-2018 Daniel Ménard
+ *
+ * For copyright and license information, please view the
+ * LICENSE.txt file that was distributed with this source code.
+ */
+namespace Docalist\Data\Tests\Export\Exporter;
+
+use Docalist\Data\Tests\Export\Exporter\DocalistJsonTest;
+use Docalist\Data\Export\Exporter\DocalistJsonPretty;
+use Docalist\Data\Export\Converter\DocalistConverter;
+use Docalist\Data\Export\Writer\JsonWriter;
+
+/**
+ * Teste l'export Docalist au format JSON Pretty.
+ *
+ * @author Daniel Ménard <daniel.menard@laposte.net>
+ */
+class DocalistJsonPrettyTest extends DocalistJsonTest
+{
+    const EXPORTER = DocalistJsonPretty::class;
+    const EXPECTED_ID = 'docalist-json-pretty';
+    const EXPECTED_CONVERTER = DocalistConverter::class;
+    const EXPECTED_WRITER = JsonWriter::class;
+    const EXPECTED_FILENAME = 'docalist-export.json';
+
+    protected function getExpectedExport()
+    {
+        $lines = [
+            '[',
+            // Enregistrement 1
+            '{',
+            '    "content": [',
+            '        {',
+            '            "type": "content",',
+            '            "value": "content"',
+            '        }',
+            '    ],',
+            '    "posttitle": "Welcome",',
+            '    "slug": "test"',
+            '},',
+
+            // L'enregistrement vide a été supprimé
+
+            // Enregistrement 2
+            '{',
+            '    "slug": "test2"',
+            '}',
+            ']',
+        ];
+
+        return implode("\n", $lines);
+    }
+}
