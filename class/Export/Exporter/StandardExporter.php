2b24ee3b26bcff30b10beb6cf9faf389778dd18d
Daniel Ménard <daniel.menard@laposte.net>
2018-04-06 14:29:07 +0100

    StandardExporter : favour composition over inheritance
    - un exporteur standard n'est pas un pipeline, il utilise un pipeline
    - ré-écriture sans hériter de StandardPipeline
    
    - le constructeur prend maintenant un Converter et un Writer, et non pas
    une liste d'opérations
    - il utilise en interne un pipeline mais celui-ci n'est ni hérité, ni
    exposé
    - ajout des méthodes appendOperation() et prependOperation()
    - ajout de la méthode getConverter()
    - suppression des méthodes setWriter()
    - implémente les méthodes supports() et getSupportDescription() ajoutées
    dans l'interface Exporter
    - utilise une constante pour la clé du converter
    - amélioration phpdoc

--- class/Export/Exporter/StandardExporter.php
+++ class/Export/Exporter/StandardExporter.php
@@ -9,20 +9,49 @@
  */
 namespace Docalist\Data\Export\Exporter;
 
-use Docalist\Pipeline\StandardPipeline;
 use Docalist\Data\Export\Exporter;
+use Docalist\Data\Export\Converter;
 use Docalist\Data\Export\Writer;
+use Docalist\Pipeline\Pipeline;
+use Docalist\Pipeline\StandardPipeline;
+use Docalist\Data\Record;
 
 /**
- * Classe de base pour les exporteurs standard.
+ * Implémentation standard de l'interface Exporter.
  *
- * Un exporteur standard utilise un pipeline de données pour convertir les enregistrements à exporter et un
- * Writer pour générer le fichier.
+ * Un exporteur standard utilise {@link Converter un convertisseur} qui effectue la conversion des
+ * {@link Record enregistrements Docalist} en données exportables et {@link Writer un Writer} qui se charge de
+ * générer le fichier d'export.
+ *
+ * En interne, la conversion est gérée par un pipeline de données. Des traitements supplémentaires peuvent être
+ * ajoutés avant ou après la conversion en utilisant les méthodes prependOperation() et appendOperation().
+ *
+ * <code>
+ * <-- input -->    <------------------------ pipeline ----------------------->    <--- writer --->    <-- output -->
+ *
+ *       Records -> [operations] -> data -> [Converter] -> data -> [operations] -> data -> [Writer] -> file.
+ *                       ^                                              ^
+ *                prependOperation()                              appendOperation()
+ * </code>
  *
  * @author Daniel Ménard <daniel.menard@laposte.net>
  */
-abstract class StandardExporter extends StandardPipeline implements Exporter
+abstract class StandardExporter implements Exporter
 {
+    /**
+     * Clé utilisée dans le pipeline pour le convertisseur.
+     *
+     * @var int
+     */
+    private const CONVERTER_KEY = 0;
+
+    /**
+     * Le pipeline utilisé pour la conversion des enregistrements.
+     *
+     * @var Pipeline
+     */
+    protected $pipeline;
+
     /**
      * Le Writer à utiliser pour générer le fichier d'export.
      *
@@ -33,27 +62,45 @@ abstract class StandardExporter extends StandardPipeline implements Exporter
     /**
      * Initialise l'exporteur.
      *
-     * @param callable[]    $operations La liste des opérations qui composent le pipeline d'export.
-     * @param Writer        $writer     Le Writer à utiliser pour générer le fichier d'export.
+     * @param Converter $converter  Le convertisseur à utiliser pour convertir les enregistrements Docalist.
+     * @param Writer    $writer     Le Writer à utiliser pour générer le fichier d'export.
      */
-    public function __construct(array $operations, Writer $writer)
+    public function __construct(Converter $converter, Writer $writer)
     {
-        parent::__construct($operations);
-        $this->setWriter($writer);
+        $this->pipeline = new StandardPipeline();
+        $this->pipeline->appendOperation($converter, self::CONVERTER_KEY);
+
+        $this->writer = $writer;
     }
 
     /**
-     * Modifie le Writer utilisé.
+     * Ajoute une opération à exécuter avant la conversion.
      *
-     * @param Writer $writer
-     *
-     * @return self
+     * @param callable $operation L'opération à ajouter.
      */
-    public function setWriter(Writer $writer): self
+    public function prependOperation(callable $operation): void
     {
-        $this->writer = $writer;
+        $this->pipeline->prependOperation($operation);
+    }
 
-        return $this;
+    /**
+     * Ajoute une opération à exécuter après la conversion.
+     *
+     * @param callable $operation L'opération à ajouter.
+     */
+    public function appendOperation(callable $operation): void
+    {
+        $this->pipeline->appendOperation($operation);
+    }
+
+    /**
+     * Retourne le convertisseur utilisé.
+     *
+     * @return Converter
+     */
+    public function getConverter(): Converter
+    {
+        return $this->pipeline->getOperation(self::CONVERTER_KEY);
     }
 
     /**
@@ -64,6 +111,16 @@ abstract class StandardExporter extends StandardPipeline implements Exporter
     public function getWriter(): Writer
     {
         return $this->writer;
+    }
+
+    public function supports(string $className): bool
+    {
+        return $this->getConverter()->supports($className);
+    }
+
+    public function getSupportDescription(): string
+    {
+        return $this->getConverter()->getSupportDescription();
     }
 
     public function getContentType(): string
@@ -83,6 +140,6 @@ abstract class StandardExporter extends StandardPipeline implements Exporter
 
     public function export(Iterable $records)
     {
-        $this->getWriter()->export($this->process($records));
+        $this->getWriter()->export($this->pipeline->process($records));
     }
 }
