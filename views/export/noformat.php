0ce8888667f3e2efdd7bb85de701ebfcb2127db0
Daniel Ménard <daniel.menard@laposte.net>
2018-04-20 14:58:03 +0200

    view/export/noformat : ré-écriture complète

--- views/export/noformat.php
+++ views/export/noformat.php
@@ -1,4 +1,4 @@
-<?php
+<?php declare(strict_types=1);
 /**
  * This file is part of Docalist Data.
  *
@@ -9,49 +9,27 @@
  */
 namespace Docalist\Data\Export\Views;
 
-use Docalist\Data\Export\Plugin;
+use Docalist\Data\Export\ExportService;
 
 /**
  * Affiche un message "aucun format disponible"
  *
- * @var Plugin  $this
- * @var array   $types Types des notices (libellé => count)
- * @var int     $total Nombre total de hits obtenus (notices à exporter).
- * @var int     $max   Nombre maximum de notices exportables.
+ * @var ExportService   $this
+ * @var int             $count Nombre total d'enregistrements dans la sélection.
  *
  * @author Daniel Ménard <daniel.menard@laposte.net>
  */
+$this->setTitle(__('Export impossible', 'docalist-data')); ?>
 
-// Crée le détail par type des notices qui seront exportées
-if (count($types) === 1) {
-    $detail = lcfirst(key($types));
-} else {
-    $detail = [];
-    foreach ($types as $label => $count) {
-        $detail[] = sprintf(__('%s : %d', 'docalist-data'), lcfirst($label), $count);
-    }
-    $detail = implode(', ', $detail);
-}
-?>
-<p>
-    Impossible d'exporter votre sélection avec les formats disponibles.
+<p class="export-intro"><?php
+    _e(
+        "Les contenus sélectionnés ne peuvent pas être exportés avec les formats actuellement disponibles.",
+        'docalist-data'
+    ); ?>
 </p>
 
-<ul>
-    <li>
-        Votre sélection contient plusieurs types de notices (<?=$detail ?>) mais
-        les formats d'export disponibles de permettent pas de tout exporter en
-        une seule étape.
-    </li>
-
-    <li>
-        Réessayez en segmentant votre sélection par type : retournez sur la page
-        précédente, utilisez la facette "type de document" pour faire une
-        sous-sélection et relancez l'export. Recommencez ensuite pour chacun des
-        types de notices à exporter.
-    </li>
-</ul>
-
-<p style="float: right">
-    <a href="javascript:history.back()">Retour à la page précédente</a>
+<p class ="export-back">
+    <a href="javascript:history.back()"><?php
+        _e('« Retour à la page précédente', 'docalist-data'); ?>
+    </a>
 </p>
