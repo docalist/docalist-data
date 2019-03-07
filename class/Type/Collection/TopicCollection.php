4a3023b435e99607993e8454f18f406ec57a9cf6
Daniel Ménard <daniel.menard@laposte.net>
2019-03-07 12:25:15 +0100

    Renomme la classe Topics en TopicCollection
    Pour que ce soit cohérent avec les autres collections.

--- class/Type/Collection/TopicCollection.php
+++ class/Type/Collection/TopicCollection.php
@@ -2,23 +2,23 @@
 /**
  * This file is part of Docalist Data.
  *
- * Copyright (C) 2012-2018 Daniel Ménard
+ * Copyright (C) 2012-2019 Daniel Ménard
  *
  * For copyright and license information, please view the
- * LICENSE.txt file that was distributed with this source code.
+ * LICENSE file that was distributed with this source code.
  */
-namespace Docalist\Data\Type;
+namespace Docalist\Data\Type\Collection;
 
 use Docalist\Type\Collection;
 use Docalist\Forms\TopicsInput;
 use Docalist\Data\Type\Topic;
 
 /**
- * Une collection de topics d'indexation.
+ * Une collection de Topic.
  *
  * @author Daniel Ménard <daniel.menard@laposte.net>
  */
-class Topics extends Collection
+class TopicCollection extends Collection
 {
     public function getEditorForm($options = null)
     {
