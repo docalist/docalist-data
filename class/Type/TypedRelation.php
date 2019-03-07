2f175ed1d51e31a36242154fd7f03a58814e092a
Daniel Ménard <daniel.menard@laposte.net>
2019-03-07 11:40:52 +0100

    TypedRelation : hérite de TypedValue plutôt que de TypedText

--- class/Type/TypedRelation.php
+++ class/Type/TypedRelation.php
@@ -2,29 +2,26 @@
 /**
  * This file is part of Docalist Data.
  *
- * Copyright (C) 2012-2018 Daniel Ménard
+ * Copyright (C) 2012-2019 Daniel Ménard
  *
  * For copyright and license information, please view the
- * LICENSE.txt file that was distributed with this source code.
+ * LICENSE file that was distributed with this source code.
  */
 namespace Docalist\Data\Type;
 
-use Docalist\Type\TypedText;
-use Docalist\Type\TableEntry;
+use Docalist\Type\TypedValue;
 use Docalist\Data\Type\Relation;
 use Docalist\Data\Record;
 use Docalist\Data\Type\Collection\TypedRelationCollection;
 
 /**
- * Une relation typée : un type composite associant un type provenant d'une table d'autorité à un champ de type
- * Relation.
+ * TypedRelation : un TypedValue qui a une valeur de type Relation.
  *
- * @property TableEntry $type   Type    Type de relation.
- * @property Relation   $value  Value   Post ID de la fiche liée.
+ * @property Relation $value Value Post ID de la fiche liée.
  *
  * @author Daniel Ménard <daniel.menard@laposte.net>
  */
-class TypedRelation extends TypedText
+class TypedRelation extends TypedValue
 {
     public static function loadSchema()
     {
