fd1b2c9037dce2835d4baa51a1dc16d406fb2d3f
Daniel Ménard <daniel.menard@laposte.net>
2019-03-07 11:53:24 +0100

    TypedPostalAddress : hérite de TypedValue plutôt que de TypedText

--- class/Type/TypedPostalAddress.php
+++ class/Type/TypedPostalAddress.php
@@ -2,27 +2,24 @@
 /**
  * This file is part of Docalist Data.
  *
- * Copyright (C) 2012-2018 Daniel Ménard
+ * Copyright (C) 2012-2019 Daniel Ménard
  *
  * For copyright and license information, please view the
- * LICENSE.txt file that was distributed with this source code.
+ * LICENSE file that was distributed with this source code.
  */
 namespace Docalist\Data\Type;
 
-use Docalist\Type\TypedText;
-use Docalist\Type\TableEntry;
+use Docalist\Type\TypedValue;
 use Docalist\Data\Type\PostalAddress;
 
 /**
- * Adresse postale typée : un type composite associant un type provenant d'une table d'autorité à une valeur
- * de type PostalAddress.
+ * TypedPostalAddress : un TypedValue qui a une valeur de type PostalAddress.
  *
- * @property TableEntry     $type   Type d'adresse (principale, secondaire...)
- * @property PostalAddress  $value  Adresse postale associée.
+ * @property PostalAddress $value Adresse postale associée.
  *
  * @author Daniel Ménard <daniel.menard@laposte.net>
  */
-class TypedPostalAddress extends TypedText
+class TypedPostalAddress extends TypedValue
 {
     public static function loadSchema()
     {
