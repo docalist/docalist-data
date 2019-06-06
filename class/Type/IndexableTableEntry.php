c9ce41704e4c2c71da189b361cb32c410d897e1b
Daniel Ménard <daniel.menard@laposte.net>
2019-06-06 12:38:18 +0200

    IndexableTableEntry : ré-écriture

--- class/Type/IndexableTableEntry.php
+++ class/Type/IndexableTableEntry.php
@@ -14,31 +14,15 @@ namespace Docalist\Data\Type;
 use Docalist\Type\TableEntry;
 use Docalist\Data\Indexable;
 use Docalist\Data\Type\Collection\IndexableCollection;
-use Docalist\Forms\Container;
-use Docalist\Search\Mapping;
-use Docalist\Data\HasIndexSettings;
+use Docalist\Data\Indexer\TableEntryIndexer;
 
 /**
- * Un TableEntry qui implémente les interfaces Indexable et HasIndexSettings.
+ * Un TableEntry qui implémente l'interface Indexable.
  *
  * @author Daniel Ménard <daniel.menard@laposte.net>
  */
-class IndexableTableEntry extends TableEntry implements Indexable, HasIndexSettings
+class IndexableTableEntry extends TableEntry implements Indexable
 {
-    /**
-     * Nom du champ de recherche.
-     *
-     * @var string
-     */
-    public const SEARCH_FIELD = 'field-name';
-
-    /**
-     * Nom du filtre.
-     *
-     * @var string
-     */
-    public const LABEL_FILTER = 'filter.field-name.label';
-
     /**
      * {@inheritDoc}
      */
@@ -50,95 +34,8 @@ class IndexableTableEntry extends TableEntry implements Indexable, HasIndexSetti
     /**
      * {@inheritDoc}
      */
-    public function getIndexSettingsForm(): Container
+    public function getIndexerClass(): string
     {
-        $form = new Container();
-
-        // Champ de recherche
-        $form->checkbox()
-            ->setName('search-field')
-            ->setLabel(static::SEARCH_FIELD)
-            ->setDescription(__('Recherche sur le code et le libellé des entrées.', 'docalist-data'));
-
-        // Filtre / facette
-        $form->checkbox()
-            ->setName('label-filter')
-            ->setLabel(static::LABEL_FILTER)
-            ->setDescription(__('Filtre sur le libellé des entrées.', 'docalist-data'));
-
-        return $form;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public function buildMapping(Mapping $mapping): void
-    {
-        // Champ de recherche
-        if ($this->getOption('search-field')) {
-            $mapping
-                ->text(static::SEARCH_FIELD)
-                ->setFeatures(Mapping::FULLTEXT)
-                ->setLabel(sprintf(
-                    __(
-                        'Recherche sur le champ "%s" des références docalist.',
-                        'docalist-data'
-                    ),
-                    $this->getSchema()->name()
-                ))
-                ->setDescription(sprintf(
-                    __(
-                        'Pour chacune des entrées du champ "%s", contient à la fois le code et le libellé
-                        qui figure dans la table d\'autorité associée au champ.',
-                        'docalist-data'
-                    ),
-                    $this->getSchema()->name()
-                ));
-        }
-
-        // Filtre/facette
-        if ($this->getOption('label-filter')) {
-            $mapping
-                ->keyword(static::LABEL_FILTER)
-                ->setFeatures(Mapping::AGGREGATE | Mapping::FILTER)
-                ->setLabel(sprintf(
-                    __(
-                        'Filtre sur le champ "%s" des références docalist.',
-                        'docalist-data'
-                    ),
-                    $this->getSchema()->name()
-                ))
-                ->setDescription(sprintf(
-                    __(
-                        'Cet attribut est un filtre sur le libellé des entrées qui figurent dans le champ "%s"
-                        des références docalist. Les libellés indexés sont ceux qui figurent dans la table
-                        d\'autorité associée au champ.',
-                        'docalist-data'
-                    ),
-                    $this->getSchema()->name()
-                ));
-        }
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    public function buildIndexData(array & $data): void
-    {
-        if (empty($code = $this->getPhpValue())) {
-            return;
-        }
-        $label = $this->getEntryLabel();
-
-        // Champ de recherche
-        if ($this->getOption('search-field')) {
-            $data[static::SEARCH_FIELD][] = $code;
-            $data[static::SEARCH_FIELD][] = $label;
-        }
-
-        // Filtre/facette
-        if ($this->getOption('label-filter')) {
-            $data[static::LABEL_FILTER][] = $label;
-        }
+        return TableEntryIndexer::class;
     }
 }
