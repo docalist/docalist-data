78a8459a0ab2896016c661ec59e53aaae975b42c
Daniel Ménard <daniel.menard@laposte.net>
2019-10-04 13:06:11 +0200

    RecordIterator : ré-écriture complète basée sur ScrollIterator
    - L'itérateur utilise maintenant l'API "scroll" de Elasticsearch (index
    "figé")
    - On hérite maintenant de ScrollIterator et on se contente de surcharger
    la méthode getIterator()
    - Le code est beaucoup plus simple !

--- class/RecordIterator.php
+++ class/RecordIterator.php
@@ -11,166 +11,53 @@ declare(strict_types=1);
 
 namespace Docalist\Data;
 
-use Docalist\Search\SearchRequest;
-use Docalist\Search\SearchResponse;
-use Iterator;
-use Countable;
+use Docalist\Search\Iterator\ScrollIterator;
+use Docalist\Data\Plugin as DocalistData;
+use Generator;
+use InvalidArgumentException;
 
 /**
  * Un itérateur de références (pour l'export).
  *
  * @author Daniel Ménard <daniel.menard@laposte.net>
  */
-class RecordIterator implements Iterator, Countable
+class RecordIterator extends ScrollIterator
 {
     /**
-     * Taille des batchs
+     * Retourne un générateur qui permet d'itérer sur les notices docalist retournés par la requête.
      *
-     * @var integer
-     */
-    const BATCH_SIZE = 1000;
-
-    /**
-     * La requête en cours.
+     * @return Generator Les clés retournées correspondent au Post_ID de la notice et la valeur associée est
+     * l'objet Record contenant la notice.
      *
-     * @var SearchRequest
+     * @return Generator
      */
-    protected $searchRequest;
-
-    /**
-     * Les résultats en cours.
-     *
-     * @var SearchResponse
-     */
-    protected $searchResponse;
-
-    /**
-     * Les hits de la page actuelle.
-     *
-     * @var array
-     */
-    protected $hits;
-
-    /**
-     * L'index du hit en cours.
-     *
-     * @var int
-     */
-    protected $current;
-
-    /**
-     * Nombre maximum de notices à itérer.
-     *
-     * @var int
-     */
-    protected $limit;
-
-    /**
-     * Nombre de hits déjà itérés.
-     *
-     * @var int
-     */
-    protected $count;
-
-    /**
-     * Construit l'itérateur.
-     *
-     * @param SearchRequest $searchRequest
-     * @param int $limit Nombre maximum de notices à itérer (zéro = pas de limite).
-     */
-    public function __construct(SearchRequest $searchRequest, int $limit = 0)
+    public function getIterator(): Generator
     {
-        $this->searchRequest = $searchRequest;
-        $this->searchRequest->setSize($limit === 0 ? self::BATCH_SIZE : min($limit, self::BATCH_SIZE));
-        $this->limit = $limit;
-        $this->count = 0;
-    }
+        // Récupère le service docalist-data utilisé pour charger les notices
+        $docalistData = docalist('docalist-data'); /** @var DocalistData $docalistData */
 
-    public function rewind()
-    {
-        $this->loadPage(1);
-    }
-
-    public function valid()
-    {
-        if ($this->current >= count($this->hits)) {
-            return false;
-        }
-
-        if (0 !== $this->limit && $this->count >= $this->limit) {
-            return false;
-        }
-
-        return true;
-    }
-
-    public function current()
-    {
-        return docalist('docalist-data')->getRecord($this->key());
-    }
-
-    public function key()
-    {
-        return $this->hits[$this->current]->_id;
-    }
-
-    public function next()
-    {
-        ++$this->current;
-        ++$this->count;
-        if (! $this->valid()) {
-            if ($this->searchResponse && $this->count < $this->searchResponse->getHitsCount()) {
-                $this->loadPage($this->searchRequest->getPage() + 1);
+        // Parcourt tous les hits et essaie de charger la notice correspondante
+        foreach (parent::getIterator() as $hit) {
+            $id = (int) $hit->_id;
+            try {
+                yield $id => $docalistData->getRecord($id);
+            } catch (InvalidArgumentException $e) {
+                // on ignore
             }
         }
-    }
 
-    /**
-     * Charge une page de résultats.
-     *
-     * @param int $page La page à charger.
-     */
-    protected function loadPage($page)
-    {
-        $this->searchRequest->setPage($page);
-        $this->searchResponse = $this->searchRequest->execute();
-        $this->hits = $this->searchResponse->getHits();
-        $this->current = 0;
-    }
-
-    /**
-     * Retourne la requête docalist-search en cours.
-     *
-     * @deprecated Utiliser getSearchRequest().
-     *
-     * @return SearchRequest
-     */
-    public function searchRequest()
-    {
-        _deprecated_function(__METHOD__, '0.15', 'getSearchRequest');
-
-        return $this->getSearchRequest();
-    }
-
-    /**
-     * Retourne la requête docalist-search en cours.
-     *
-     * @return SearchRequest
-     */
-    public function getSearchRequest()
-    {
-        return $this->searchRequest;
-    }
-
-    /**
-     * Retourne le nombre de notices dans l'itérateur.
-     *
-     * @return int
-     */
-    public function count()
-    {
-        is_null($this->searchResponse) && $this->rewind();
-
-        return $this->searchResponse->getHitsCount();
+        /*
+         * getRecord() génère une exception dans deux cas :
+         *
+         * 1. Le post n'existe pas : ça peut se produire car on utilise une requête scroll (index "figé"),
+         *    et il se peut que la notice correspondant au hit retourné ait été supprimée pendant le scroll.
+         *    Dans ce cas on ignore l'erreur.
+         *
+         * 2. Le post indiqué n'est pas un record docalist. Cela ne devrait pas se produire car avant
+         *    d'utiliser un RecordIterator, on doit s'assurer qu'on n'a que des Record dans la requête
+         *    (en ajoutant des filtres sur les types).
+         *    On ne devrait pas ignorer l'erreur dans ce cas, mais comme on ne peut pas distinguer les
+         *    deux cas, on ignore tout de même.
+         */
     }
 }
