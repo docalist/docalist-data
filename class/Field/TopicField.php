3876c6ac51fe8bf3afc09ba8d0e5423f6a0aa7ae
Daniel Ménard <daniel.menard@laposte.net>
2020-01-29 15:28:07 +0100

    TopicField : gère les liens rebonds

--- class/Field/TopicField.php
+++ class/Field/TopicField.php
@@ -15,6 +15,7 @@ use Docalist\Data\Type\Topic;
 use Docalist\Data\Indexable;
 use Docalist\Data\Type\Collection\IndexableTopicCollection;
 use Docalist\Data\Indexer\TopicFieldIndexer;
+use Docalist\Forms\Container;
 
 /**
  * Champ standard "topic" : Une liste de mots-clés d'un certain type.
@@ -62,5 +63,50 @@ class TopicField extends Topic implements Indexable
     public function getIndexerClass(): string
     {
         return TopicFieldIndexer::class;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getFormatSettingsForm(): Container
+    {
+        $form = parent::getFormatSettingsForm();
+
+        $form->checkbox('searchlink')
+            ->setLabel(__('Liens rebonds', 'docalist-data'))
+            ->setDescription(__(
+                "Génère un lien de recherche pour chaque mot-clé
+                (l'attribut <code>filter.topic.label</code> doit être actif).",
+                'docalist-data'
+            ));
+
+        return $form;
+    }
+
+
+    /**
+     * {@inheritDoc}
+     */
+    final protected function getFormattedTerms($options): array
+    {
+        $terms = $this->getTermsLabel();
+
+        if ($this->getOption('searchlink', $options)) {
+            $baseUrl = apply_filters('docalist_search_get_search_page_url', '');
+            if (!empty($baseUrl)) {
+                foreach ($terms as $code => $label) {
+                    $url = $baseUrl . '?filter.topic.label=' . urlencode($label);
+                    $title = __("Rechercher ce mot-clé", 'docalist-data');
+                    $terms[$code] = sprintf(
+                        '<a class="searchlink" href="%s" title="%s">%s</a>',
+                        esc_attr($url),
+                        esc_attr($title),
+                        $label
+                    );
+                }
+            }
+        }
+
+        return $terms;
     }
 }
