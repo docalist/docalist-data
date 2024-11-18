691dc6a5991b25da631c9021de979488f26f5b03
Daniel Ménard <daniel.menard.35@gmail.com>
2024-11-18 20:13:19 +0100

    autoload et déclaration de l'extension kernel

---
+++ bootstrap.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * This file is part of Docalist Data.
+ *
+ * Copyright (C) 2012-2019 Daniel Ménard
+ *
+ * For copyright and license information, please view the
+ * LICENSE file that was distributed with this source code.
+ *
+ * @author Daniel Ménard <daniel.menard@laposte.net>
+ */
+declare(strict_types=1);
+
+namespace Docalist\Data;
+
+use Docalist\Data\DocalistDataExtension;
+use Docalist\Kernel\Kernel;
+
+Kernel::registerExtension(DocalistDataExtension::class);
