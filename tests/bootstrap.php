1e5efa2365b1589f0efa957163d5d1eeb85e6b08
Daniel Ménard <daniel.menard.35@gmail.com>
2024-11-18 20:11:39 +0100

    Tests : simplifie bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -9,17 +9,19 @@
  */
 declare(strict_types=1);
 
-namespace Docalist\Data\Test;
+require_once __DIR__ . '/../vendor/autoload.php';
 
-// Environnement de test
-$GLOBALS['wp_tests_options'] = array(
-    'active_plugins' => array(
-        'docalist-core/docalist-core.php',
-        'docalist-data/docalist-data.php',
-        'docalist-search/docalist-search.php',
-    ),
-);
+// namespace Docalist\Data\Test;
 
-// wordpress-tests doit être dans le include_path de php
-// sinon, modifier le chemin d'accès ci-dessous
-require_once 'wordpress-develop/tests/phpunit/includes/bootstrap.php';
+// // Environnement de test
+// $GLOBALS['wp_tests_options'] = array(
+//     'active_plugins' => array(
+//         'docalist-core/docalist-core.php',
+//         'docalist-data/docalist-data.php',
+//         'docalist-search/docalist-search.php',
+//     ),
+// );
+
+// // wordpress-tests doit être dans le include_path de php
+// // sinon, modifier le chemin d'accès ci-dessous
+// require_once 'wordpress-develop/tests/phpunit/includes/bootstrap.php';
